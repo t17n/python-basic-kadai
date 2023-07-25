8ecaaca3780f2c45d35625cdf3c5b1d5e6b002d2
t17n <140472930+t17n@users.noreply.github.com>
2023-07-25 16:42:09 +0900

    kadai_005.py修正
    VSCodeで出力数値をアップロードできなかったため、
    PowerShellの出力内容も併せて提出させて頂きます。

--- kadai_005.py
+++ kadai_005.py
@@ -1,3 +1,4 @@
+VSCode
 ue = 10
 shita = 20
 takasa = 5
@@ -6,3 +7,18 @@ daikei = (ue + shita ) * takasa / 2
 
 print(f"{daikei}㎠")
 
+----------------------------------------
+
+PowerShell 7.3.6
+PS C:\Windows\System32> python
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> ue = 10
+>>> shita = 20
+>>> takasa = 5
+>>>
+>>> daikei = (ue + shita ) * takasa / 2
+>>>
+>>> print(f"{daikei}㎠")
+75.0㎠
+>>>
